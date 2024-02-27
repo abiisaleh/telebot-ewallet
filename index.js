9f80422c7d80d34734bca95b0663e6ce13223fb4
abiisaleh <abi.saleh.25@gmail.com>
2024-02-27 17:07:31 +0700

    add express

--- index.js
+++ index.js
@@ -2,56 +2,71 @@ require('dotenv').config()
 
 const { Telegraf } = require('telegraf') 
 const { message } = require('telegraf/filters')
+const express = require('express')
+const app = express(); 
+const PORT = 3000; 
+  
+app.get('/', (req, res)=>{ 
+    res.status(200); 
+    res.send("Welcome to root URL of Server");
+}); 
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+app.listen(PORT, (error) =>{ 
+  if(!error) {
+    const bot = new Telegraf(process.env.BOT_TOKEN)
+    const api = 'https://api-rekening.lfourr.com'
 
-const api = 'https://api-rekening.lfourr.com'
-
-async function getEwallet(){
-  const response = await fetch(`${api}/listEwallet`)
-  const data = await response.json()
-  return data
-}
-
-async function getAccountName(bank, telp){
-  const response = await fetch(`${api}/getEwalletAccount?bankCode=${bank}&accountNumber=${telp}`)
-  const data = await response.json()
-  return data
-}
-
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('kirim nomor telp yang mau di cek akun ewallet-nya'))
-
-bot.on(message('text'), async (ctx) => {
-
-  const telp = ctx.message.text
-  const idCode = /^(\+62|62|0)(\d{8,15})$/
-
-  // validasi nomor telp
-  if (!idCode.test(telp)) {
-    return await ctx.reply(`nomor yang anda masukkan salah`)
-  }
-
-  // Using context shortcut
-  let msg = '';
-  const ewallet = await getEwallet()
-
-  for (const bank of ewallet.data) {
-    const pemilik = await getAccountName(bank.kodeBank, telp)
-
-    try {
-      msg += `${bank.kodeBank} : ${pemilik.data.accountname} \n`
-    } catch (err) {
-      //
+    async function getEwallet(){
+      const response = await fetch(`${api}/listEwallet`)
+      const data = await response.json()
+      return data
     }
+
+    async function getAccountName(bank, telp){
+      const response = await fetch(`${api}/getEwalletAccount?bankCode=${bank}&accountNumber=${telp}`)
+      const data = await response.json()
+      return data
+    }
+
+    bot.start((ctx) => ctx.reply('Welcome'))
+    bot.help((ctx) => ctx.reply('kirim nomor telp yang mau di cek akun ewallet-nya'))
+
+    bot.on(message('text'), async (ctx) => {
+
+      const telp = ctx.message.text
+      const idCode = /^(\+62|62|0)(\d{8,15})$/
+
+      // validasi nomor telp
+      if (!idCode.test(telp)) {
+        return await ctx.reply(`nomor yang anda masukkan salah`)
+      }
+
+      // Using context shortcut
+      let msg = '';
+      const ewallet = await getEwallet()
+
+      for (const bank of ewallet.data) {
+        const pemilik = await getAccountName(bank.kodeBank, telp)
+
+        try {
+          msg += `${bank.kodeBank} : ${pemilik.data.accountname} \n`
+        } catch (err) {
+          //
+        }
+      }
+
+      await ctx.reply(`${msg}`)
+
+    })
+
+    bot.launch()
+
+    // Enable graceful stop
+    process.once('SIGINT', () => bot.stop('SIGINT'))
+    process.once('SIGTERM', () => bot.stop('SIGTERM'))
+    console.log("Server is Successfully Running, and App is listening on port "+ PORT) 
   }
-
-  await ctx.reply(`${msg}`)
-
-})
-
-bot.launch()
-
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+  else 
+    console.log("Error occurred, server can't start", error); 
+  } 
+);
